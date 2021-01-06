2cb63ca6b4c9abcd6acb5ba7aa52fa3c84c7459a
“KR-AirDrop” <choish4682@naver.com>
2021-01-06 13:02:40 +0900

    atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: seonchoi <seonchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/06 12:45:48 by seonchoi          #+#    #+#             */
-/*   Updated: 2021/01/06 12:45:49 by seonchoi         ###   ########.fr       */
+/*   Updated: 2021/01/06 13:02:28 by seonchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int		ft_atoi(const char *str)
 	nbr = 0;
 	if (!str[i])
 		return (0);
-	while (ft_isspace(str[i]))
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
 		i += 1;
 	if (str[i] == '-' || str[i] == '+')
 		if (str[i++] == '-')
