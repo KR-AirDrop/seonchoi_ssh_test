a094c37e6dc78e8948f6b8ecf6ed71d48fd028a1
“KR-AirDrop” <choish4682@naver.com>
2021-01-11 18:08:25 +0900

    char

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: seonchoi <seonchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 17:17:33 by seonchoi          #+#    #+#             */
-/*   Updated: 2021/01/11 18:07:32 by seonchoi         ###   ########.fr       */
+/*   Updated: 2021/01/11 18:08:20 by seonchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (ft_strdup(""));
 	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	ft_strlcpy(str, (char)s + start, len + 1);
+	ft_strlcpy(str, (char *)s + start, len + 1);
 	return (str);
 }
