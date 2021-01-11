87693b1c7bee55f00e9f25da93b83d468d009e61
“KR-AirDrop” <choish4682@naver.com>
2021-01-11 22:10:07 +0900

    strjoin

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seonchoi <seonchoi@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/11 22:02:30 by seonchoi          #+#    #+#             */
+/*   Updated: 2021/01/11 22:09:32 by seonchoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char *str;
+
+	if (!(str = (char *)malloc(sizeof(char) * ft_strlen(s1) + ft_strlen(s2) + 1)))
+		return (NULL);
+	ft_memcpy(str, s1, ft_strlen(s1));
+	ft_memcpy(str + ft_strlen(s1), s2, ft_strlen(s2));
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	return (str);
+}
