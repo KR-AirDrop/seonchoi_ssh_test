4d36ee689a2cb530851e7aa03b0fe8cf5406fbb2
“KR-AirDrop” <choish4682@naver.com>
2021-01-11 17:58:44 +0900

    norm

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,32 +6,22 @@
 /*   By: seonchoi <seonchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 17:17:33 by seonchoi          #+#    #+#             */
-/*   Updated: 2021/01/11 17:54:52 by seonchoi         ###   ########.fr       */
+/*   Updated: 2021/01/11 17:58:22 by seonchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_substr(char const *s, unsigned int start, size_t len)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t i;
-	char *str;
+	char *substr;
 
-    if (!s)
-    	return (NULL);
-    if (ft_strlen(s) < start)
-    	return (ft_strdup(""));
-	if (start > 0)
-		start--;
-	i = 0;
-	if (!(str = (char *)malloc(sizeof(char) * len + 1)))
+	if (!s)
 		return (NULL);
-	while (s[start] && i < len)
-	{
-		str[i] = s[start];
-		i++;
-		start++;
-	}
-	str[i] = '\0';
-	return (str);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	if (!(substr = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	ft_strlcpy(substr, s + start, len + 1);
+	return (substr);
 }
