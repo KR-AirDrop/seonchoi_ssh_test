58923de1f69fd8843eaeaaa68e6434c1269bc987
“KR-AirDrop” <choish4682@naver.com>
2021-01-15 14:03:21 +0900

    *

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,13 +6,13 @@
 /*   By: seonchoi <seonchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 13:37:36 by seonchoi          #+#    #+#             */
-/*   Updated: 2021/01/15 14:00:36 by seonchoi         ###   ########.fr       */
+/*   Updated: 2021/01/15 14:03:05 by seonchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_front(t_list *lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	new->next = *lst;
 	*lst = new;
