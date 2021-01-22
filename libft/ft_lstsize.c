7ecaedd13a214091762e02df0a6f18484fbe071a
“KR-AirDrop” <choish4682@naver.com>
2021-01-22 20:43:26 +0900

    size

--- libft/ft_lstsize.c
+++ libft/ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: seonchoi <seonchoi@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/15 14:01:21 by seonchoi          #+#    #+#             */
-/*   Updated: 2021/01/22 15:22:22 by seonchoi         ###   ########.fr       */
+/*   Updated: 2021/01/22 20:42:49 by seonchoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int		ft_lstsize(t_list *lst)
 	int	size;
 
 	size = 0;
+	if (lst == NULL)
+		return (0);
 	while (lst != NULL)
 	{
 		lst = lst->next;
